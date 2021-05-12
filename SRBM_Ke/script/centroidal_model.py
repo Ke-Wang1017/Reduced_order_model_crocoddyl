c5b4b0dca05833bd379727ac0def40adf3c325a5
kewang <superwk1017@gmail.com>
2021-05-12 14:51:43 +0100

    modify the centroidal model

--- SRBM_Ke/script/centroidal_model.py
+++ SRBM_Ke/script/centroidal_model.py
@@ -6,7 +6,8 @@ import utils
 
 class DifferentialActionModelCentroidal(crocoddyl.DifferentialActionModelAbstract):
     def __init__(self, costs, m):
-        crocoddyl.DifferentialActionModelAbstract.__init__(self, crocoddyl.StateVector(12), 12) # nu = 12
+        # x: pos, orientation, lin_vel, ang_vel
+        crocoddyl.DifferentialActionModelAbstract.__init__(self, crocoddyl.StateVector(12), 6) # nu = 6 = 3*2 feet
         self.uNone = np.zeros(self.nu)
 
         self.m = m
@@ -15,29 +16,29 @@ class DifferentialActionModelCentroidal(crocoddyl.DifferentialActionModelAbstrac
         self.g = np.zeros(6)
         self.g[2] = -9.81
         self.costs = costs
+        # by row: xyz; by column: contact point
         self.footholds = np.array(
-            [[0.19, 0.19, -0.19, -0.19],
-             [0.15005, -0.15005, 0.15005, -0.15005],
-             [0.0, 0.0, 0.0, 0.0]])
+            [[0.0, 0.0],
+             [0.085, -0.085],
+             [0.0, 0.0]])
         #Normal vector for friction cone
         self.nsurf = np.array([0., 0., 1.]).T # flat ground
-        self.S = np.ones(4)
+        self.S = np.ones(2)# I want to use bipedal robot at first
 
-        # self.u_lb = np.array([100., -0.8, -0.12, 0.0])
-        # self.u_ub = np.array([2.0*self.m*self.g, 0.8, 0.12, 0.05])
-
+        self.u_lb = np.array([-2.0*self.m*self.g, -2.0*self.m*self.g, 0.0, -2.0*self.m*self.g, -2.0*self.m*self.g, 0.0])
+        self.u_ub = np.array([2.0*self.m*self.g, 2.0*self.m*self.g, 2.0*self.m*self.g, 2.0*self.m*self.g, 2.0*self.m*self.g, 2.0*self.m*self.g])
 
     def calc(self, data, x, u):
-        # Levers Arms used in B, can I put lever-arms into data???
-        data.L = self.footholds - np.array(x[:3]).transpose() # broadcast x
-        H = utils.euler_matrix(x[3],x[4],x[5])
+        # Levers Arms used in B
+        data.Level_arm = self.footholds - np.array(x[:3]).transpose() # broadcast x
+        H = utils.euler_matrix(x[3],x[4],x[5]) # rotation matrix
         R = H[:3,:3]
         data.I_inv = np.linalg.inv(np.dot(R, self.gI))
-        for i in range(4):
+        for i in range(2):
             # if feet in touch with ground
             if self.S[i] != 0:
                 data.B[:3, (i*3):((i+1)*3)] = np.identity(3)/self.m #
-                data.B[-3:, (i*3):((i+1)*3)] = np.dot(data.I_inv, utils.getSkew(data.L[:, i])) # another term needs added
+                data.B[-3:, (i*3):((i+1)*3)] = np.dot(data.I_inv, utils.getSkew(data.Level_arm[:, i])) # another term needs added, for now it is OK
         # Compute friction cone
         # self.costFriction(u)
         data.xout = np.dot(data.B,u) + self.g
@@ -47,7 +48,7 @@ class DifferentialActionModelCentroidal(crocoddyl.DifferentialActionModelAbstrac
         data.cost = data.costs.cost
 
     def calcDiff(self, data, x, u):
-        for i in range(4):
+        for i in range(2):
             if self.S[i] != 0:
                 data.derivative_B[-3:, 0] = - np.dot(data.I_inv, np.cross([1, 0, 0], [u[3 * i], u[3 * i + 1],
                                                                                                 u[3 * i + 2]]))  # \x
@@ -61,13 +62,13 @@ class DifferentialActionModelCentroidal(crocoddyl.DifferentialActionModelAbstrac
 
     def createData(self):
         data = DifferentialActionDataCentroidal(self)
-        data.B = np.zeros((6, 12))
-        data.L = np.zeros((3, 4)) # 4 contact points
-        data.derivative_B = np.zeros((6, 12))
+        data.B = np.zeros((12, 6))
+        data.Level_arm = np.zeros((3, 2)) # 2 contact points
+        data.derivative_B = np.zeros((12, 6))
         data.I_inv = np.identity(3)
         return data
 
-    def updateModel(self, foothold, nsurf, contact_selection, Ig):
+    def updateModel(self, foothold, nsurf, contact_selection, Ig=np.diag([0.00578574, 0.01938108, 0.02476124])):
         self.footholds = foothold
         self.nsurf = nsurf
         self.S = contact_selection
@@ -80,29 +81,27 @@ class DifferentialActionDataCentroidal(crocoddyl.DifferentialActionDataAbstract)
         self.costs = model.costs.createData(shared_data)
         self.costs.shareMemory(self)
 
-
-def createPhaseModel(foothold, nsurf, mu, xref=np.array([0.0, 0.0, 0.86, 0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0]),
-                     Wx=np.ones(12), Wu=np.ones(12), wxreg=1, wureg=5, wufriction=5, wxbox=1, dt=2e-2):
+def createPhaseModel(foothold, contact, xref=np.array([0.0, 0.0, 0.86, 0.0, 0.0, 0.0]), nsurf=np.array([0.,0.,1.]).T, mu=0.7, Wx=np.array([0., 0., 10., 10., 10., 10.]), Wu=np.array([0., 50., 50., 1.]),
+                     wxreg=1, wureg=5, wutrack=50, wxbox=1, dt=2e-2):
     state = crocoddyl.StateVector(12)
-    runningCosts = crocoddyl.CostModelSum(state, 12)
-    Foothold = foothold
-    Nsurf = nsurf
-    Mu = mu
+    runningCosts = crocoddyl.CostModelSum(state, 6)
+    uRef = np.hstack([np.zeros(1), foothold])# ???
     xRef = xref
-    cone = crocoddyl.FrictionCone(Nsurf, Mu, 4, False)
-    x_ub = np.hstack([Foothold, np.zeros(3)]) + np.array([0.3, 0.055, 0.95, 7., 7., 3]) # can be modified
-    x_lb = np.hstack([Foothold, np.zeros(3)]) + np.array([-0.3, -0.055, 0.75, -7., -7., -3])
-    u_lb = np.tile(cone.lb, 4)  # force based on friction cone
-    u_ub = np.tile(cone.ub, 4)
-    runningCosts.addCost("comBox", crocoddyl.CostModelState(state, crocoddyl.ActivationModelQuadraticBarrier(crocoddyl.ActivationBounds(x_lb, x_ub)), xRef, 4), wxbox)
-    runningCosts.addCost("comReg", crocoddyl.CostModelState(state, crocoddyl.ActivationModelWeightedQuad(Wx), xRef, 12), wxreg)
-    runningCosts.addCost("frictionCone", crocoddyl.CostModelControl(state, crocoddyl.ActivationModelQuadraticBarrier((crocoddyl.ActivationBounds(u_lb, u_ub)))), wufriction)
-    runningCosts.addCost("uReg", crocoddyl.CostModelControl(state, 12), wureg*10) ## ||u||^2
-    model = DifferentialActionModelCentroidal(runningCosts, m=95.0)
+    nSurf = nsurf
+    Mu = mu
+    # cone = crocoddyl.FrictionCone(nSurf, Mu, 1, False)
+    ub = np.hstack([foothold, np.zeros(3)]) + np.array([0.3, 0.055, 0.95, 7., 7., 3])
+    lb = np.hstack([foothold, np.zeros(3)]) + np.array([-0.3, -0.055, 0.75, -7., -7., -3])
+    runningCosts.addCost("comBox", crocoddyl.CostModelResidual(state, crocoddyl.ActivationModelQuadraticBarrier(crocoddyl.ActivationBounds(lb, ub)), crocoddyl.ResidualModelState(state, xRef, 6)), wxbox)
+    runningCosts.addCost("comReg", crocoddyl.CostModelResidual(state, crocoddyl.ActivationModelWeightedQuad(Wx), crocoddyl.ResidualModelState(state, xRef, 6)), wxreg)
+    runningCosts.addCost("uTrack", crocoddyl.CostModelControl(state, crocoddyl.ActivationModelWeightedQuad(Wu), uRef), wureg) ## ||u||^2
+    runningCosts.addCost("uReg", crocoddyl.CostModelResidual(state, crocoddyl.ResidualModelControl(state,6)), wutrack) ## ||u||^2
+    model = DifferentialActionModelCentroidal(runningCosts)
+    model.updateModel(foothold=foothold,contact_selection=contact)
     return crocoddyl.IntegratedActionModelEuler(model, dt)
 
-def createTerminalModel(cop):
-    return createPhaseModel(cop, xref=np.array([0.0, 0.0, 0.86, 0.02, 0.0, 0.0]), Wx=np.array([0., 0., 100., 30., 30., 150.]), wxreg=1e6, dt=0.)
+def createTerminalModel(foothold):
+    return createPhaseModel(foothold, xref=np.array([0.0, 0.0, 0.86, 0.0, 0.0, 0.0]), Wx=np.array([0., 0., 100., 30., 30., 150.]), wxreg=1e6, dt=0.)
 
 m1 = createPhaseModel(np.array([0.0, 0.0, 0.00]))
 m2 = createPhaseModel(np.array([0.0, -0.08, 0.00]))
@@ -111,8 +110,12 @@ m4 = createPhaseModel(np.array([0.2, 0.08, 0.00]))
 m5 = createPhaseModel(np.array([0.2, 0.0, 0.00]))
 mT = createTerminalModel(np.array([0.2, 0.0, 0.00]))
 
+foot_holds = np.array([[0.0, 0.0, 0.0],[0.0, -0.08, 0.05],[0.1, 0.0, 0.1],[0.2, 0.08, 0.15],[0.2, 0.0, 0.2]])
+phase = np.array([0, 1, 0, -1, 0]) # 0: double, 1: left, -1: right
+
 num_nodes_single_support = 50
-num_nodes_double_support = 20
+num_nodes_double_support = 25
+
 locoModel = [m1]*num_nodes_double_support
 locoModel += [m2]*num_nodes_single_support
 locoModel += [m3]*num_nodes_double_support
