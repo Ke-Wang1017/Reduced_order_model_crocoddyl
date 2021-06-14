664a535a7f7003e750fe9bbd18152f3fa1e72b57
kewang <superwk1017@gmail.com>
2021-06-14 11:27:53 +0100

    add constraints

--- Variable_Height_Crocoddyl/scripts/util.py
+++ Variable_Height_Crocoddyl/scripts/util.py
@@ -2,21 +2,21 @@ import numpy as np
 from math import cos,sin
 
 def rotx(theta):
-    Rx = np.vstack(np.hstack(1, 0, 0),
-                 np.hstack(0, cos(theta), -sin(theta)),
-                 np.hstack(0, sin(theta), cos(theta)))
+    Rx = np.array([[1, 0, 0],
+                  [0, cos(theta), -sin(theta)],
+                 [0, sin(theta), cos(theta)]])
     return Rx
 
 
 def roty(theta):
-    Ry = np.vstack(np.hstack(cos(theta), 0, sin(theta)),
-                 np.hstack(0, 1, 0),
-                 np.hstack(-sin(theta), 0, cos(theta)))
+    Ry = np.array([[cos(theta), 0, sin(theta)],
+                 [0, 1, 0],
+                 [-sin(theta), 0, cos(theta)]])
     return Ry
 
 
 def rotz(theta):
-    Rz = np.vstack(np.hstack(cos(theta), -sin(theta), 0),
-                 np.hstack(sin(theta), cos(theta), 0),
-                 np.hstack(0, 0, 1))
+    Rz = np.array([[cos(theta), -sin(theta), 0],
+                 [sin(theta), cos(theta), 0],
+                 [0, 0, 1]])
     return Rz
