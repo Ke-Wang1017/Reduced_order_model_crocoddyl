fd4a96984880803ec3d7e677d258c5aba5028a55
Carlos Mastalli <carlos.mastalli@ed.ac.uk>
2021-07-14 16:48:22 +0100

    [cone] Improved efficiency of asymmetric friction cone

--- ResidualAsymmetricFrictionConeClass.py
+++ ResidualAsymmetricFrictionConeClass.py
@@ -4,6 +4,42 @@ from math import sqrt
 from util import rotRollPitchYaw
 
 
+class AsymmetricFrictionConeResidual(crocoddyl.ResidualModelAbstract):
+    def __init__(self, state, nu, cone):
+        crocoddyl.ResidualModelAbstract.__init__(self, state, 4, nu, True, False, True)
+        self.cone = cone
+
+    def calc(self, data, x, u):
+        data.p[:] = x[:3] - data.shared.actuation.tau
+        data.f[0] = data.p[0] / data.p[2]
+        data.f[1] = data.p[1] / data.p[2]
+        data.r[0] = -self.cone.xp.dot(data.f)
+        data.r[1] = -self.cone.xn.dot(data.f)
+        data.r[2] = -self.cone.yp.dot(data.f)
+        data.r[3] = -self.cone.yn.dot(data.f)
+
+    def calcDiff(self, data, x, u):
+        xn, xp, yn, yp = self.cone.xn, self.cone.xp, self.cone.yn, self.cone.yp
+        data.tmp_pz2 = data.p[2]**2
+        data.Rx[:, :3] = \
+            -np.array([[xp[0] / data.p[2], xp[1] / data.p[2], -xp[0] * data.p[0] / data.tmp_pz2 - xp[1] * data.p[1] / data.tmp_pz2],
+                       [xn[0] / data.p[2], xn[1] / data.p[2], -xn[0] * data.p[0] / data.tmp_pz2 - xn[1] * data.p[1] / data.tmp_pz2],
+                       [yp[0] / data.p[2], yp[1] / data.p[2], -yp[0] * data.p[0] / data.tmp_pz2 - yp[1] * data.p[1] / data.tmp_pz2],
+                       [yn[0] / data.p[2], yn[1] / data.p[2], -yn[0] * data.p[0] / data.tmp_pz2 - yn[1] * data.p[1] / data.tmp_pz2]])
+        data.Ru[:,:] = -data.Rx[:, :3].dot(data.shared.actuation.dtau_du)
+
+    def createData(self, collector):
+        return AsymmetricFrictionConeDataResidual(self, collector)
+
+class AsymmetricFrictionConeDataResidual(crocoddyl.ResidualDataAbstract):
+    def __init__(self, model, collector):
+        crocoddyl.ResidualDataAbstract.__init__(self, model, collector)
+        self.p = np.zeros(3)
+        self.f = np.zeros(3)
+        self.f[2] = 1.
+        self.tmp_pz2 = 0.
+
+
 class AsymmetricCone:
     def __init__(self, mu, ori):
         self.mu = mu
@@ -53,42 +89,3 @@ class AsymmetricCone:
             self.yn = yn_l
         else:
             self.yn = yn_r
-
-class AsymmetricFrictionConeResidual(crocoddyl.ResidualModelAbstract):
-    def __init__(self, state, nu, cone):
-        crocoddyl.ResidualModelAbstract.__init__(self, state, 4, nu, True, False, True)
-        self.cone = cone
-
-    def calc(self, data, x, u):
-        data.p[:] = x[:3] - data.shared.actuation.tau
-        data.f[0] = data.p[0] / data.p[2]
-        data.f[1] = data.p[1] / data.p[2]
-        data.r[0] = -self.cone.xp.dot(data.f)
-        data.r[1] = -self.cone.xn.dot(data.f)
-        data.r[2] = -self.cone.yp.dot(data.f)
-        data.r[3] = -self.cone.yn.dot(data.f)
-
-    def calcDiff(self, data, x, u):
-        data.Rx[:, :3] = \
-            -np.array([[self.cone.xp[0] / data.p[2], self.cone.xp[1] / data.p[2], -self.cone.xp[0] * data.p[0] / (data.p[2]**2) - self.cone.xp[1] * data.p[1] / (data.p[2])**2],
-                       [self.cone.xn[0] / data.p[2], self.cone.xn[1] / data.p[2], -self.cone.xn[0] * data.p[0] / (data.p[2]**2) - self.cone.xn[1] * data.p[1] / (data.p[2])**2],
-                       [self.cone.yp[0] / data.p[2], self.cone.yp[1] / data.p[2], -self.cone.yp[0] * data.p[0] / (data.p[2]**2) - self.cone.yp[1] * data.p[1] / (data.p[2])**2],
-                       [self.cone.yn[0] / data.p[2], self.cone.yn[1] / data.p[2], -self.cone.yn[0] * data.p[0] / (data.p[2]**2) - self.cone.yn[1] * data.p[1] / (data.p[2])**2]])
-        data.dr_dtau[:, :] = \
-            -np.array([[-self.cone.xp[0] / data.p[2], -self.cone.xp[1] / data.p[2], self.cone.xp[0] * data.p[0] / (data.p[2])**2 + self.cone.xp[1] * data.p[1] / (data.p[2])**2],
-                       [-self.cone.xn[0] / data.p[2], -self.cone.xn[1] / data.p[2], self.cone.xn[0] * data.p[0] / (data.p[2])**2 + self.cone.xn[1] * data.p[1] / (data.p[2])**2],
-                       [-self.cone.yp[0] / data.p[2], -self.cone.yp[1] / data.p[2], self.cone.yp[0] * data.p[0] / (data.p[2])**2 + self.cone.yp[1] * data.p[1] / (data.p[2])**2],
-                       [-self.cone.yn[0] / data.p[2], -self.cone.yn[1] / data.p[2], self.cone.yn[0] * data.p[0] / (data.p[2])**2 + self.cone.yn[1] * data.p[1] / (data.p[2])**2]])
-        data.Ru[:,:] = data.dr_dtau.dot(data.shared.actuation.dtau_du)
-
-    def createData(self, collector):
-        return AsymmetricFrictionConeDataResidual(self, collector)
-
-class AsymmetricFrictionConeDataResidual(crocoddyl.ResidualDataAbstract):
-    def __init__(self, model, collector):
-        crocoddyl.ResidualDataAbstract.__init__(self, model, collector)
-        self.dr_dtau = np.zeros((4,3))
-        self.p = np.zeros(3)
-        self.f = np.zeros(3)
-        self.f[2] = 1.
-
